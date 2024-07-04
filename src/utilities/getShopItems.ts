a442170d229ad58acbd0fee1d222fd6773e86174
Xirlorm <xirlorm@gmail.com>
2024-07-04 20:37:29 +0000

    Store data fetch api modification

--- src/utilities/getShopItems.ts
+++ src/utilities/getShopItems.ts
@@ -1,11 +1,11 @@
-async function getItems(args: string = '') {
+async function fetchData(args: string = '') {
   try {
-    const response = await fetch(`https://fakestoreapi.com/products/${args}`)
+    const response = await fetch(`https://fakestoreapi.com/${args}`)
     
     if (!response.ok) {
-      throw new Error('Error fetching data')
+      throw new Error('An error occured while fetching data')
     }
-    
+  
     const json = await response.json()
     return json
   } catch(error) {
@@ -13,4 +13,4 @@ async function getItems(args: string = '') {
   }
 }
 
-export default getItems
+export default fetchData
