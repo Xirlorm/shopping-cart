c1c3749b7b5e36992c3be1b3abe9d5a558e596b2
Xirlorm <xirlorm@gmail.com>
2024-07-14 21:53:11 +0000

    applied Prettier code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import styles from './styles/App.module.css'
-import AppContext from './context'
-import { Outlet } from 'react-router-dom'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import { useState } from 'react'
+import styles from "./styles/App.module.css";
+import AppContext from "./context";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { useState } from "react";
 
 function App() {
-  const [itemsInCart, setItemsInCart] = useState(0)
+  const [itemsInCart, setItemsInCart] = useState(0);
 
   return (
-    <AppContext.Provider value={{
-      itemsInCart,
-      setItemsInCart,
-    }}>
+    <AppContext.Provider
+      value={{
+        itemsInCart,
+        setItemsInCart,
+      }}
+    >
       <div className={styles.container}>
         <Header />
-        <main>
+        <main className={styles.main}>
           <Outlet />
         </main>
         <Footer />
       </div>
     </AppContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,20 @@
-import App from './App.tsx'
-import Cart from './pages/Cart.tsx'
-import Home from './pages/Home.tsx'
-import NotFound from './pages/NotFound.tsx'
-import Shop from './pages/Shop.tsx'
+import App from "./App.tsx";
+import Cart from "./pages/Cart.tsx";
+import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
+import Shop from "./pages/Shop.tsx";
 
-const router = [{
-  path: '/',
-  element: <App />,
-  errorElement: <NotFound />,
-  children: [
-    { index: true, element: <Home /> },
-    { path: 'shop', element: <Shop /> },
-    { path: 'cart', element: <Cart /> },
-  ],
-}]
+const router = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "shop", element: <Shop /> },
+      { path: "cart", element: <Cart /> },
+    ],
+  },
+];
 
-export default router
+export default router;
