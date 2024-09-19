80927ea9c1fb00d0ff58a8ae9f1fde3934d41f33
Xirlorm <xirlorm@gmail.com>
2024-09-19 20:40:57 +0000

    Updated tests to begin from router roots and not App

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,11 +1,20 @@
 import { screen, render } from "@testing-library/react";
 import React from "react";
-import App from '../src/App';
 import { describe, expect, it } from "vitest";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from '../src/lib/routes'
+
+const router = createBrowserRouter(routes)
+const app = <RouterProvider router={router} />
 
 describe('App', () => {
-  it('renders headline', () => {
-    render(<App />);
-    expect(screen.getByRole('heading').textContent).toMatch(/Shopping cart/i);
+  it('Renders Header', () => {
+    render(app);
+    expect(screen.getByRole('banner', {name: 'header'})).toBeInTheDocument();
+  })
+
+  it('Renders Footer', () => {
+    render(app);
+    expect(screen.getByRole('contentinfo', {name: 'footer'})).toBeInTheDocument();
   })
 });
