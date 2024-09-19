4bc6fdcf0a6e358b3bf57883ce2723e22377e030
Xirlorm <xirlorm@gmail.com>
2024-09-19 20:35:25 +0000

    Added Cart page

--- src/lib/routes.tsx
+++ src/lib/routes.tsx
@@ -1,6 +1,7 @@
 import App from "../App";
 import Home from "../layouts/Home";
 import Shop from "../layouts/Shop";
+import Cart from "../layouts/Cart";
 import UnexpectedError from "../layouts/UnexpectedError";
 
 export default [
@@ -11,6 +12,7 @@ export default [
     children: [
       { index: true, element: <Home /> },
       { path: 'shop', element: <Shop /> },
+      { path: 'cart', element: <Cart />}
     ],
   },
 ]
