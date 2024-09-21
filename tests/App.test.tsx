5d95ca71dd013fb71b0ede454f1d569df08d0959
Xirlorm <xirlorm@gmail.com>
2024-09-21 20:55:52 +0000

    Added header nav click event tests

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,20 +1,49 @@
 import { screen, render } from "@testing-library/react";
 import React from "react";
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it } from "vitest";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from '../src/lib/routes'
+import userEvent from "@testing-library/user-event";
 
 const router = createBrowserRouter(routes)
 const app = <RouterProvider router={router} />
 
-describe('App', () => {
+beforeEach(() => render(app));
+
+// Header and Footer rendering tests
+describe('Header and footer renders', () => {
   it('Renders Header', () => {
-    render(app);
-    expect(screen.getByRole('banner', {name: 'header'})).toBeInTheDocument();
+    const header = screen.getByRole('banner', {name: 'header'});
+    expect(header).toBeInTheDocument();
   })
 
   it('Renders Footer', () => {
-    render(app);
-    expect(screen.getByRole('contentinfo', {name: 'footer'})).toBeInTheDocument();
+    const footer = screen.getByRole('contentinfo', {name: 'footer'})
+    expect(footer).toBeInTheDocument();
   })
 });
+
+
+// Header navigation tests
+describe("Header navigation", () => {
+  it('Clicking Shop icon in header loads Shop page', async () => {
+    const user = userEvent.setup();
+    const shopIcon = screen.getByRole('link', {name: 'shop'});
+    await user.click(shopIcon);
+    expect(screen.getByRole('main')).toHaveClass('shop');
+  })
+
+  it('Clicking Cart icon in header loads Cart page', async () => {
+    const user = userEvent.setup();
+    const shopIcon = screen.getByRole('link', {name: 'cart'});
+    await user.click(shopIcon);
+    expect(screen.getByRole('main')).toHaveClass('cart');
+  })
+
+  it('Clicking Home icon in header loads Home page', async () => {
+    const user = userEvent.setup();
+    const shopIcon = screen.getByRole('link', {name: 'home'});
+    await user.click(shopIcon);
+    expect(screen.getByRole('main')).toHaveClass('home');
+  })
+})
