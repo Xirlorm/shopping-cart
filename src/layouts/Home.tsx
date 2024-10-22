7af65141315dc1dd398233250d55fbacd3d41bf5
Xirlorm <xirlorm@gmail.com>
2024-10-22 10:14:54 +0000

    A few updates to home component

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import styles from '../styles/app.module.css';
+import { getProducts } from '../lib/utilities';
+
 function Home() {
+  useEffect(() => {
+    getProducts().then(data => console.log(data))
+  })
+
   return (
-    <main className="home">
+    <main className={styles.main + " home"}>
       <h2>Home page</h2>
     </main>
   )
