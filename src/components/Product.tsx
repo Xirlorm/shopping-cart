85b286e7dac6114b34a14b439f41caaf27b1a0bd
Xirlorm <xirlorm@gmail.com>
2024-10-29 20:30:54 +0000

    Show change in count of a product added/removed

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,12 +1,15 @@
 import { Minus, Plus } from "react-feather";
 import { ProductType } from "../lib/types"
 import styles from '../styles/components.module.css'
+import { useState } from "react";
 
 interface ProductArg {
   product: ProductType
 }
 
 function Product({product}: ProductArg) {
+  let [amountPicked, setAmountPicked] = useState(0);
+
   return (
     <div className={styles.product}>
       <img src={product.image} alt="" />
@@ -14,9 +17,13 @@ function Product({product}: ProductArg) {
         <h4>{product.title}</h4>
         <li>${product.price}.00</li>
         <div className={styles.addToCart + " addToCart"}>
-          <button><Plus /></button>
-          <input type="number" placeholder="0" value={0} />
-          <button><Minus /></button>
+          <button onClick={() => {setAmountPicked(val => val + 1)}}>
+            <Plus />
+          </button>
+          <input type="number" placeholder="0" value={amountPicked} />
+          <button onClick={() => {setAmountPicked(val => val > 0 ? val - 1 : val)}}>
+            <Minus />
+          </button>
         </div>
       </div>
     </div>
